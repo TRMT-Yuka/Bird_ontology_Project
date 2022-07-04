7bc9dd02ba995c2497fda0762e9fdc99fae23314
TRMT-Yuka <momo144.art@gmail.com>
2022-07-04 13:20:13 +0900

    make all_ID.html

--- sh6_absent_p_taxon.sh
+++ sh6_absent_p_taxon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -sLH 'Accept: application/json' http://www.wikidata.org/entity/Q52762328 | jq . > data_json_13/Q52762328.json
-curl -sLH 'Accept: application/json' http://www.wikidata.org/entity/Q23809240 | jq . > data_json_13/Q23809240.json
+curl -sLH 'Accept: application/json' http://www.wikidata.org/entity/Q160830 | jq . > data_json_13/Q160830.json
+curl -sLH 'Accept: application/json' http://www.wikidata.org/entity/Q2043179 | jq . > data_json_13/Q2043179.json
 curl -sLH 'Accept: application/json' http://www.wikidata.org/entity/None | jq . > data_json_13/None.json
